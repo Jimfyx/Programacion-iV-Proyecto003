5e7c7ca3b1d324346e81af6dd1d74479fe69f0c2
Jim Navarro <jimfyx@gmail.com>
2024-08-27 21:32:18 -0600

    productor ready

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(DemoApplication.class, args);
-    }
 
+        List<Integer> cola = new ArrayList<>();
+
+        Thread productor = new Thread(new Productor(cola));
+
+        productor.start();
+
+        productor.join();
+
+        System.out.println(cola.size());
+    }
 }
--- demo/src/main/java/com/example/demo/Productor.java
+++ demo/src/main/java/com/example/demo/Productor.java
@@ -1,9 +1,34 @@
 package com.example.demo;
 
-public class Productor implements Runnable {
+import java.util.List;
+import java.util.Random;
 
+public class Productor implements Runnable {
+    private final List<Integer> cola;
+
+    public Productor(List<Integer> cola) {
+        this.cola = cola;
+    }
     @Override
     public void run() {
-        
+        Random random = new Random();
+
+                    try {
+                        for (int i = 0; i < 1000; i++) {
+                            synchronized (cola) {
+                                while (cola.size() == 1000) {
+                                    cola.wait();
+                                }
+                                int num = random.nextInt(100) + 1;
+                                cola.add(num);
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+
+
+                    }
+
+
     }
 }
